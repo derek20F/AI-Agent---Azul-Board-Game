40f9eba01db439b7475eb8d5c1d4c8693496b49e
chenanf <chenanf@student.unimelb.edu.au>
2020-06-03 17:41:33 +1000

    BFS agent

--- players/Azul_project_group_21/BFS.py
+++ players/Azul_project_group_21/BFS.py
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
+# 1066384, Yu-Ting Liu
+
 from advance_model import *
 import utils
 import copy
